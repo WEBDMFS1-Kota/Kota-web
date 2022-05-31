11423b9b56848330fe837a9be3e79be84e21ef40
Damokless <damokless@protonmail.com>
2022-05-31 15:28:58 +0200

    chore: adding react router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root'),
+);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
+  </BrowserRouter>,
 );
