c80caef071c9a3b795bfbb051cf3fa1be1d43209
Damokless <damokless@protonmail.com>
2022-06-01 10:49:29 +0200

    feat: add two navbar, one before login and one after

---
+++ src/components/NavbarBeforeLogin.js
@@ -0,0 +1,59 @@
+import React from 'react';
+
+function NavbarBeforeLogin() {
+  return (
+    <header className="bg-gray-900">
+      <div className="max-w-screen-xl px-4 mx-auto sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="md:flex md:items-center md:gap-12">
+            <a className="block text-white" href="/">
+              <span>Kota</span>
+            </a>
+          </div>
+          <div className="hidden md:block">
+            <nav aria-labelledby="header-navigation">
+              <h2 className="sr-only" id="header-navigation">Header navigation</h2>
+              <ul className="flex items-center gap-6 text-sm">
+                <li>
+                  <a className="text-white transition hover:text-white/75" href="/">
+                    Hot
+                  </a>
+                </li>
+                <li>
+                  <a className="text-white transition hover:text-white/75" href="/">
+                    Top
+                  </a>
+                </li>
+                <li>
+                  <a className="text-white transition hover:text-white/75" href="/">
+                    All projects
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className="flex items-center gap-4">
+            <div className="sm:gap-4 sm:flex">
+              <a
+                className="px-5 py-2.5 text-sm font-medium text-white bg-blue-600 rounded-md shadow"
+                href="/"
+              >
+                Login
+              </a>
+              <div className=" sm:flex">
+                <a
+                  className="px-5 py-2.5 text-sm font-medium text-white bg-gray-800 rounded-md"
+                  href="/"
+                >
+                  Register
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default NavbarBeforeLogin;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,26 +2,7 @@ import React from 'react';
 
 function Home() {
   return (
-    <section className="text-white bg-gray-900">
-      <div className="max-w-screen-xl px-4 py-32 mx-auto lg:h-screen lg:items-center lg:flex">
-        <div className="max-w-3xl mx-auto text-center">
-          <h1 className="text-3xl font-extrabold text-transparent sm:text-5xl bg-clip-text bg-gradient-to-r from-green-300 via-blue-500 to-purple-600">
-            Kota
-          </h1>
-          <p className="max-w-xl mx-auto mt-4 sm:leading-relaxed sm:text-xl">
-            learning from others.
-          </p>
-          <div className="flex flex-wrap justify-center gap-4 mt-8">
-            <a className="block w-full px-12 py-3 text-sm font-medium text-white bg-blue-600 border border-blue-600 rounded sm:w-auto active:text-opacity-75 hover:bg-transparent hover:text-white focus:outline-none focus:ring" href="/get-started">
-              Get Started
-            </a>
-            <a className="block w-full px-12 py-3 text-sm font-medium text-white border border-blue-600 rounded sm:w-auto hover:bg-blue-600 active:bg-blue-500 focus:outline-none focus:ring" href="/about">
-              Learn More
-            </a>
-          </div>
-        </div>
-      </div>
-    </section>
+    <p>Kota</p>
   );
 }
 
