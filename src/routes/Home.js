498b83e039eff447410906004781a2335ac2d534
Damokless <damokless@protonmail.com>
2022-06-05 00:35:00 +0200

    feat: creating  design project card

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHotjar } from '@fortawesome/free-brands-svg-icons';
-import { faRankingStar } from '@fortawesome/free-solid-svg-icons';
+import { faRankingStar, faArrowDown, faArrowUp } from '@fortawesome/free-solid-svg-icons';
 
 import NavbarBeforeLogin from '../components/NavbarBeforeLogin';
 import NavbarAfterLogin from '../components/NavbarAfterLogin';
@@ -19,7 +19,7 @@ function Home() {
     <>
       <StatusLogin islogged={display} />
       <div className="flex justify-center">
-        <div className="bg-gray-700 mt-6 rounded py-3 flex justify-center w-96">
+        <div className="bg-gray-700 mt-6 rounded shadow-xl py-3 flex justify-center w-96">
           <div className="flex">
             <button type="button" className="rounded-xl hover:bg-gray-600 py-1.5 px-2.5 mr-4">
               <div className="flex items-center justify-center">
@@ -36,6 +36,27 @@ function Home() {
                   Top
                 </span>
               </div>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 my-10 mx-12 justify-items-center">
+        <div className="bg-gray-700 text-white w-96 rounded-lg overflow-hidden shadow-xl my-2">
+          <img className="w-full" src="https://tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains" />
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+            <p className="text-grey-darker text-base">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla!
+            </p>
+          </div>
+          <div className="flex px-6 py-4">
+            <button type="button" className=" rounded-xl hover:bg-gray-600 py-1.5 px-2.5">
+              <span className="inline-block bg-grey-lighter rounded-full px-1 py-1 text-sm font-semibold text-grey-darker mr-2"><FontAwesomeIcon icon={faArrowUp} /></span>
+              <span className="inline-block bg-grey-lighter rounded-full text-sm font-semibold text-grey-darker">420</span>
+            </button>
+            <button type="button" className="rounded-xl hover:bg-gray-600 py-1.5 px-2.5">
+              <span className="inline-block bg-grey-lighter rounded-full px-1 py-1 text-sm font-semibold text-grey-darker mr-2"><FontAwesomeIcon icon={faArrowDown} /></span>
+              <span className="inline-block bg-grey-lighter rounded-full text-sm font-semibold text-grey-darker">3</span>
             </button>
           </div>
         </div>
