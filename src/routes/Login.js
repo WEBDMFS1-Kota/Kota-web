cf8549ba02f60e632004b426ba9a19de41d7117c
Elouan MAILLY <leloupandco@gmail.com>
2022-06-06 15:40:05 +0200

    chore: disable submit behavior on all validation buttons
    Signed-off-by: Elouan MAILLY <leloupandco@gmail.com>

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -72,7 +72,7 @@ function Login() {
             </Link>
           </div>
           <button
-            type="submit"
+            type="button"
             className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Login
