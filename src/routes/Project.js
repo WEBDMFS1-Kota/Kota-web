b67705506630bebb56604cfe4430572d6add2763
Elouan MAILLY <leloupandco@gmail.com>
2022-06-20 01:40:21 +0200

    feat: Project page logic (#42)
    * feat: fetch infos and display project info
    
    Signed-off-by: Elouan MAILLY <leloupandco@gmail.com>
    
    * chore: add fetchCreatorInfo()
    
    Signed-off-by: Elouan MAILLY <leloupandco@gmail.com>

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactMarkdown from 'react-markdown';
+import { useParams, useNavigate } from 'react-router-dom';
 import remarkGfm from 'remark-gfm';
 import 'github-markdown-css';
 import '../styles/project.css';
@@ -7,6 +8,40 @@ import { StatusLogin } from './Home';
 
 function Project() {
   const display = false;
+  const { projectID } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [project, setProject] = useState({});
+
+  const navigate = useNavigate();
+
+  async function fetchCreatorInfo() {
+    console.log(project);
+  }
+  async function fetchProjectInfos() {
+    const request = await fetch(`https://kota-api-prod.herokuapp.com/projects/${projectID}`, { method: 'GET' });
+    if (request.status === 200) {
+      const response = await request.json();
+      setProject(response);
+      await fetchCreatorInfo();
+    } else {
+      navigate('/project/notfound', { replace: true });
+    }
+  }
+
+  useEffect(() => {
+    fetchProjectInfos().then(() => setLoading(false));
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="flex flex-col min-h-screen">
+        <StatusLogin islogged={display} />
+        <section className="min-h-full flex flex-grow items-center justify-center">
+          <h1 className="text-3xl text-white">Loading...</h1>
+        </section>
+      </div>
+    );
+  }
 
   return (
     <div>
@@ -14,10 +49,12 @@ function Project() {
       <section className="text-white bg-gray-900">
         <div className="w-1/2 mx-auto">
           <div className="py-10 text-center">
-            <h1 className="text-6xl">Title of project</h1>
+            <h1 className="text-6xl">{project.title}</h1>
           </div>
           <div>
             <ReactMarkdown
+                /* eslint-disable-next-line react/no-children-prop */
+              children={project.description}
               remarkPlugins={[remarkGfm]}
               className="markdown-body"
             />
