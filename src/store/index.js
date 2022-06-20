3137cc4a0c3a8af4daa66c8280e18bf5902c610c
Elouan MAILLY <leloupandco@gmail.com>
2022-06-20 17:49:22 +0200

    feat: add redux-persist to save in localStorage store
    Signed-off-by: Elouan MAILLY <leloupandco@gmail.com>

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,33 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import userReducer from './userSlice';
 
-export default configureStore({
-  reducer: {
-    user: userReducer,
-  },
+const persistConfig = {
+  key: 'kota',
+  storage,
+};
+
+const reducers = combineReducers({ user: userReducer });
+const persistedReducer = persistReducer(persistConfig, reducers);
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
 });
+
+export const persistor = persistStore(store);
+
+export default store;
