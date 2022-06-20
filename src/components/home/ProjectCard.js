b02de737e23b089d1f2c68931bd71b494cf2347d
Damokless <damokless@protonmail.com>
2022-06-20 20:27:38 +0200

    chore: assign functions to the respective buttons

--- src/components/home/ProjectCard.js
+++ src/components/home/ProjectCard.js
@@ -44,11 +44,11 @@ function projectCard(props) {
           </p>
         </div>
         <div className="flex px-6 py-4">
-          <button type="button" className=" rounded-xl hover:bg-gray-600 py-1.5 px-2.5">
+          <button type="button" className=" rounded-xl hover:bg-gray-600 py-1.5 px-2.5" onClick={() => upVote()}>
             <span className="inline-block bg-grey-lighter rounded-full px-1 py-1 text-sm font-semibold text-grey-darker mr-2"><FontAwesomeIcon icon={faArrowUp} /></span>
             <span className="inline-block bg-grey-lighter rounded-full text-sm font-semibold text-grey-darker">{project.upVote}</span>
           </button>
-          <button type="button" className="rounded-xl hover:bg-gray-600 py-1.5 px-2.5">
+          <button type="button" className="rounded-xl hover:bg-gray-600 py-1.5 px-2.5" onClick={() => downVote()}>
             <span className="inline-block bg-grey-lighter rounded-full px-1 py-1 text-sm font-semibold text-grey-darker mr-2"><FontAwesomeIcon icon={faArrowDown} /></span>
             <span className="inline-block bg-grey-lighter rounded-full text-sm font-semibold text-grey-darker">{project.downVote}</span>
           </button>
