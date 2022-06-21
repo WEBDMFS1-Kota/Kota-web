7842d3dbe15b683c3d627f1af7b739ebfa7f3978
Elouan MAILLY <leloupandco@gmail.com>
2022-06-21 14:45:23 +0200

    feat: Create project full logic (#49)
    * feat: add default logic for get input and fetch tags from API
    
    Signed-off-by: Elouan MAILLY <leloupandco@gmail.com>
    
    * feat: add loading of md file, not working right now
    
    Signed-off-by: Elouan MAILLY <leloupandco@gmail.com>
    
    * feat: complete load of README file
    
    Signed-off-by: Elouan MAILLY <leloupandco@gmail.com>
    
    * feat: complete creation of project
    
    Signed-off-by: Elouan MAILLY <leloupandco@gmail.com>
    
    * feat: add S3 interface to implement upload file
    
    Signed-off-by: Elouan MAILLY <leloupandco@gmail.com>
    
    * feat: complete upload of project image
    
    Signed-off-by: Elouan MAILLY <leloupandco@gmail.com>

--- src/routes/AddProject.js
+++ src/routes/AddProject.js
@@ -1,13 +1,10 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import SimpleMDE from 'react-simplemde-editor';
 import 'easymde/dist/easymde.min.css';
 import Select from 'react-select';
-
-const options = [
-  { value: 'NodeJS', label: 'NodeJS' },
-  { value: 'Python', label: 'Python' },
-  { value: 'vanilla', label: 'Vanilla' },
-];
+import uploadImage from '../services/S3Service';
 
 const customStyles = {
   menu: (provided) => ({
@@ -39,6 +36,102 @@ const customStyles = {
 };
 
 function AddProject() {
+  const [projectTitle, setProjectTitle] = useState('');
+  const [projectDescription, setProjectDescription] = useState('');
+  const [projectImage, setProjectImage] = useState('');
+  const [globalTags, setGlobalTags] = useState([]);
+  const [projectTags, setProjectTags] = useState([]);
+
+  const token = useSelector((state) => state.user.token);
+  const navigate = useNavigate();
+
+  const ProjectDescChange = useCallback((value) => {
+    setProjectDescription(value);
+  }, []);
+
+  async function fetchTags() {
+    const response = await fetch('https://kota-api-prod.herokuapp.com/tags', { method: 'GET' });
+    if (response.ok) {
+      const tags = await response.json();
+      const tempoTagArray = [];
+      tags.forEach((tag) => {
+        tempoTagArray.push({
+          value: tag.id,
+          label: tag.name,
+        });
+      });
+      setGlobalTags(tempoTagArray);
+    }
+  }
+
+  function loadMDFile() {
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.accept = '.md,.txt';
+    input.onchange = (e) => {
+      const file = e.target.files[0];
+
+      const reader = new FileReader();
+      reader.readAsText(file, 'UTF-8');
+
+      reader.onload = (readerEvent) => {
+        const content = readerEvent.target.result;
+        setProjectDescription(content);
+      };
+    };
+    input.click();
+  }
+
+  function selectImage() {
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.accept = '.jpg,.jpeg,.png,.svg,.webp,.gif,.avif';
+    input.onchange = (e) => {
+      const file = e.target.files[0];
+
+      uploadImage('projectImages', file).then((imageURL) => {
+        if (imageURL) {
+          setProjectImage(imageURL);
+        }
+      });
+    };
+    input.click();
+  }
+
+  async function createProject() {
+    const sentProjectTags = [];
+
+    projectTags.forEach((projectTag) => {
+      sentProjectTags.push({
+        id: projectTag.value,
+        name: projectTag.label,
+      });
+    });
+
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        title: projectTitle,
+        description: projectDescription,
+        projectTags: sentProjectTags,
+      }),
+    };
+    const response = await fetch('https://kota-api-prod.herokuapp.com/projects', requestOptions);
+    if (response.ok) {
+      const { id } = await response.json();
+      navigate(`/project/${id}`, { replace: false });
+    }
+  }
+
+  useEffect(() => {
+    fetchTags();
+  }, []);
+
   return (
     <section className="text-white bg-gray-900">
       <div className="max-w-screen-xl px-4 py-32 mx-auto lg:h-screen">
@@ -49,13 +142,17 @@ function AddProject() {
         </div>
         <div className="my-10">
           <div className="text-right">
-            <button type="button" className="mx-2 p-2 border-2 border-white rounded-lg">
+            <button type="button" className="mx-2 p-2 border-2 border-white rounded-lg" onClick={loadMDFile}>
               Import a README file
             </button>
             <button type="button" className="mx-2 p-2 border-2 border-white rounded-lg">
               Preview result
             </button>
-            <button type="button" className="mx-2 p-2 border-2 border-white rounded-lg">
+            <button
+              type="button"
+              className="mx-2 p-2 border-2 border-white rounded-lg"
+              onClick={createProject}
+            >
               Create project
             </button>
           </div>
@@ -72,9 +169,22 @@ function AddProject() {
               id="projectTitle"
               className="bg-transparent border border-black text-black sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-white dark:placeholder-gray-200 dark:text-white mt-3"
               placeholder="Name of the project"
+              onChange={(e) => setProjectTitle(e.target.value)}
               required=""
             />
           </label>
+        </div>
+        <div className="mt-4 w-full">
+          <h1
+            className="text-lg font-medium text-gray-900 block dark:text-gray-300 mb-5"
+          >
+            Image of the project (optional)
+          </h1>
+          <div className="w-full text-center">
+            <img src={projectImage} alt="projectImage" className="mb-4 mx-auto" />
+            <button type="button" className="mx-auto p-2 border-2 border-white rounded-lg" onClick={selectImage}>Upload image</button>
+          </div>
+          <input type="hidden" onChange={(e) => setProjectImage(e.target.value)} />
         </div>
         <div className="mt-4">
           <h1
@@ -82,15 +192,17 @@ function AddProject() {
           >
             Description of the project
           </h1>
-          <SimpleMDE className="bg-transparent" />
+          <SimpleMDE className="bg-transparent" onChange={ProjectDescChange} value={projectDescription} />
         </div>
-        <div>
+        <div className="mb-32">
           <h1 className="text-lg font-medium text-gray-900 block dark:text-gray-300 mb-5">
             Tags of the project
           </h1>
           <Select
             isMulti
-            options={options}
+            closeMenuOnSelect={false}
+            onChange={(e) => setProjectTags(e)}
+            options={globalTags}
             styles={customStyles}
           />
         </div>
---
+++ src/services/S3Service.js
@@ -0,0 +1,46 @@
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import { CognitoIdentityClient } from '@aws-sdk/client-cognito-identity';
+import { fromCognitoIdentityPool } from '@aws-sdk/credential-provider-cognito-identity';
+import { v4 as uuidv4 } from 'uuid';
+
+let client = null;
+
+function S3Interface() {
+  if (!client) {
+    const region = 'eu-west-3';
+    client = new S3Client({
+      region,
+      credentials: fromCognitoIdentityPool({
+        client: new CognitoIdentityClient({ region }),
+        identityPoolId: 'eu-west-3:141666ba-d6a2-4760-812a-f1d68c8ca26e', // IDENTITY_POOL_ID
+      }),
+    });
+  }
+  return client;
+}
+
+async function uploadImage(folderPath, file) {
+  const uploadInterface = await S3Interface();
+
+  const fileName = uuidv4();
+  const fileKey = `${folderPath}/${fileName}.${file.name.split('.')[1]}`;
+
+  const uploadParams = {
+    Bucket: 'kota-s3-prod',
+    Key: fileKey,
+    Body: file,
+  };
+
+  console.log(uploadParams);
+
+  try {
+    const data = await uploadInterface.send(new PutObjectCommand(uploadParams));
+    console.log(data);
+    return `https://kota-s3-prod.s3.eu-west-3.amazonaws.com/${fileKey}`;
+  } catch (err) {
+    console.error('There was an error uploading your photo: ', err.message);
+    return null;
+  }
+}
+
+export default uploadImage;
