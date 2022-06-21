36795da30f1886a057daa3274ec696f735c05c6e
Elouan MAILLY <leloupandco@gmail.com>
2022-06-22 00:11:54 +0200

    chore: add joke create project button in user profile
    Signed-off-by: Elouan MAILLY <leloupandco@gmail.com>

--- src/components/User/UserInfos.js
+++ src/components/User/UserInfos.js
@@ -67,27 +67,34 @@ function UserInfos(props) {
                   )
               }
           </div>
-          <div className="mt-2 flex flex-row items-center space-x-5">
-            <a
-              href="#projects"
-              className="flex h-20 w-40 flex-col items-center justify-center rounded-md border border-gray-200 transition-colors duration-100 ease-in-out hover:border-gray-400/80"
-            >
-              <div className="flex flex-row items-center justify-center">
-                <svg
-                  className="mr-3 fill-gray-400/95"
-                  version="1.1"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    d="M2.5 19.6L3.8 20.2V11.2L1.4 17C1 18.1 1.5 19.2 2.5 19.6M15.2 4.8L20.2 16.8L12.9 19.8L7.9 7.9V7.8L15.2 4.8M15.3 2.8C15 2.8 14.8 2.8 14.5 2.9L7.1 6C6.4 6.3 5.9 7 5.9 7.8C5.9 8 5.9 8.3 6 8.6L11 20.5C11.3 21.3 12 21.7 12.8 21.7C13.1 21.7 13.3 21.7 13.6 21.6L21 18.5C22 18.1 22.5 16.9 22.1 15.9L17.1 4C16.8 3.2 16 2.8 15.3 2.8M10.5 9.9C9.9 9.9 9.5 9.5 9.5 8.9S9.9 7.9 10.5 7.9C11.1 7.9 11.5 8.4 11.5 8.9S11.1 9.9 10.5 9.9M5.9 19.8C5.9 20.9 6.8 21.8 7.9 21.8H9.3L5.9 13.5V19.8Z"
-                  />
-                </svg>
-                <span className="font-bold text-gray-400">{projectsNumber}</span>
-              </div>
-              <div className="mt-2 text-sm text-gray-400">Projects</div>
-            </a>
+          <div className="flex flex-row">
+            <div className="mt-2 flex flex-row items-center space-x-5">
+              <a
+                href="#projects"
+                className="flex h-20 w-40 flex-col items-center justify-center rounded-md border border-gray-200 transition-colors duration-100 ease-in-out hover:border-gray-400/80"
+              >
+                <div className="flex flex-row items-center justify-center">
+                  <svg
+                    className="mr-3 fill-gray-400/95"
+                    version="1.1"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      d="M2.5 19.6L3.8 20.2V11.2L1.4 17C1 18.1 1.5 19.2 2.5 19.6M15.2 4.8L20.2 16.8L12.9 19.8L7.9 7.9V7.8L15.2 4.8M15.3 2.8C15 2.8 14.8 2.8 14.5 2.9L7.1 6C6.4 6.3 5.9 7 5.9 7.8C5.9 8 5.9 8.3 6 8.6L11 20.5C11.3 21.3 12 21.7 12.8 21.7C13.1 21.7 13.3 21.7 13.6 21.6L21 18.5C22 18.1 22.5 16.9 22.1 15.9L17.1 4C16.8 3.2 16 2.8 15.3 2.8M10.5 9.9C9.9 9.9 9.5 9.5 9.5 8.9S9.9 7.9 10.5 7.9C11.1 7.9 11.5 8.4 11.5 8.9S11.1 9.9 10.5 9.9M5.9 19.8C5.9 20.9 6.8 21.8 7.9 21.8H9.3L5.9 13.5V19.8Z"
+                    />
+                  </svg>
+                  <span className="font-bold text-gray-400">{projectsNumber}</span>
+                </div>
+                <div className="mt-2 text-sm text-gray-400">Projects</div>
+              </a>
+            </div>
+            <div>
+              <NavLink to="/project/add">
+                <img src={`${process.env.PUBLIC_URL}/bouton_create_project.jpg`} alt="creatProjectButton" />
+              </NavLink>
+            </div>
           </div>
         </div>
         {
