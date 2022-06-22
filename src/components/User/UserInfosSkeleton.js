2f587114a623392ed817ef6d668284f42b05353b
Elouan MAILLY <leloupandco@gmail.com>
2022-06-22 12:18:31 +0200

    feat: implement skeleton for userInfos and userProjects
    Signed-off-by: Elouan MAILLY <leloupandco@gmail.com>

--- src/components/User/UserInfosSkeleton.js
+++ src/components/User/UserInfosSkeleton.js
@@ -1,5 +1,30 @@
-function UserInfosSkeleton() {
+import React from 'react';
 
+function UserInfosSkeleton() {
+  return (
+    <div className="px-4">
+      <div className="flex flex-row rounded-lg border border-gray-600/80 bg-gray-700 mt-6 p-6 shadow-xl">
+        <div className="relative">
+          <div className="w-40 h-40 bg-slate-800 rounded-full object-cover animate-pulse" />
+        </div>
+        <div className="flex flex-col px-6 w-1/3">
+          <div className="">
+            <div className="w-full h-12 bg-slate-800 animate-pulse" />
+          </div>
+          <div className="my-2 flex flex-col">
+            <div className="flex flex-row">
+              <div className="w-1/2 h-6 bg-slate-800 animate-pulse" />
+            </div>
+            <div className="w-full animate-pulse" />
+            <div className="flex flex-row">
+              <div className="w-4/5 mt-1 h-6 bg-slate-800 animate-pulse" />
+            </div>
+            <div className="w-3/4 h-6 mt-1 bg-slate-800 animate-pulse" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default UserInfosSkeleton;
--- src/components/User/UserProjectsSkeleton.js
+++ src/components/User/UserProjectsSkeleton.js
@@ -1,5 +1,39 @@
-function UserProjectsSkeleton() {
+import React from 'react';
+import { faArrowDown, faArrowUp } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+function UserProjectsSkeleton() {
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
+      {
+        [...Array(4)].map(() => (
+          <div className="px-4 mt-2">
+            <div className="bg-gray-700 text-white rounded-lg overflow-hidden shadow-xl my-4">
+              <div className="px-6 py-4">
+                <div className="w-2/3 h-10 bg-slate-800 animate-pulse" />
+                <div className="w-full h-6 mt-2 bg-slate-800 animate-pulse" />
+                <div className="w-1/2 h-6 bg-slate-800 animate-pulse" />
+              </div>
+              <div className="grid grid-cols-2 px-6 py-4 gap-x-1">
+                <button type="button" className=" rounded-xl hover:bg-gray-600 py-1.5">
+                  <div className="grid grid-cols-2">
+                    <span className="bg-grey-lighter rounded-full py-1 text-sm font-semibold text-grey-darker"><FontAwesomeIcon icon={faArrowUp} /></span>
+                    <div className="h-6 w-3/4 bg-slate-800 animate-pulse my-auto" />
+                  </div>
+                </button>
+                <button type="button" className="rounded-xl hover:bg-gray-600 py-1.5">
+                  <div className="grid grid-cols-2">
+                    <span className="inline-block bg-grey-lighter rounded-full px-1 py-1 text-sm font-semibold text-grey-darker"><FontAwesomeIcon icon={faArrowDown} /></span>
+                    <div className="h-6 w-3/4 bg-slate-800 animate-pulse my-auto" />
+                  </div>
+                </button>
+              </div>
+            </div>
+          </div>
+        ))
+        }
+    </div>
+  );
 }
 
 export default UserProjectsSkeleton;
