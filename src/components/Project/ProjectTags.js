3ebcaf9bf4cd27a0066e490e2f2056ed7ea2d389
Elouan MAILLY <leloupandco@gmail.com>
2022-06-22 23:15:46 +0200

    fix: projectTags are too close
    Signed-off-by: Elouan MAILLY <leloupandco@gmail.com>

--- src/components/Project/ProjectTags.js
+++ src/components/Project/ProjectTags.js
@@ -6,7 +6,7 @@ function ProjectTags(props) {
     <div className="flex">
       {
           projectTags.map((tag) => (
-            <div className="py-1 px-3 bg-blue-500 rounded-full">
+            <div className="py-1 px-3 mx-1 bg-blue-500 rounded-full">
               <p>{tag.name}</p>
             </div>
           ))
--- src/components/Project/skeletons/ProjectTagsSkeleton.js
+++ src/components/Project/skeletons/ProjectTagsSkeleton.js
@@ -5,7 +5,7 @@ function ProjectTagsSkeleton() {
     <div className="flex">
       {
         [...Array(5)].map((value, index) => (
-          <div className="py-1 px-3 bg-blue-500 rounded-full h-8 w-24 animate-pulse" key={`projectTagsSkeleton-${index - 1}`} />
+          <div className="py-1 px-3 mx-1 bg-blue-500 rounded-full h-8 w-24 animate-pulse" key={`projectTagsSkeleton-${index - 1}`} />
         ))
       }
     </div>
