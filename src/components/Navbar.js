d9c2bdc2266e931b0a37ff2745e453982320d22f
Elouan MAILLY <leloupandco@gmail.com>
2022-06-23 23:06:46 +0200

    fix: dropdown menu not disappear when click

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,6 +13,11 @@ function Navbar() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
+  function dropdownItemClick(path) {
+    document.activeElement?.blur();
+    navigate(path);
+  }
+
   function signOut() {
     dispatch(logout());
     navigate('/', { replace: true });
@@ -37,7 +42,7 @@ function Navbar() {
         isLogged
           && (
           <div aria-label="toggler" className="flex justify-center items-center">
-            <div className=" relative inline-block text-left dropdown">
+            <div className=" relative inline-block text-left dropdown" id="dropdownMenu">
               <button
                 className="inline-flex justify-center w-full leading-5 transition duration-150 ease-in-out rounded-md focus:outline-none"
                 type="button"
@@ -51,8 +56,8 @@ function Navbar() {
               <div className="opacity-0 invisible dropdown-menu transition-all duration-300 transform origin-top-right -translate-y-2 scale-95">
                 <div className="absolute right-0 w-56 mt-2 origin-top-right bg-gray-400 dark:bg-gray-900 border border-gray-600 divide-y divide-gray-500 rounded-md shadow-lg outline-none" aria-labelledby="headlessui-menu-button-1" id="headlessui-menu-items-117" role="menu">
                   <div className="py-1">
-                    <NavLink to={`/user/${userID}`} className="flex justify-between w-full px-4 py-2 text-sm leading-5 text-left hover:bg-gray-500 hover:dark:bg-gray-700 hover:text-white" role="menuitem">View profile</NavLink>
-                    <NavLink to="/userSettings" className="flex justify-between w-full px-4 py-2 text-sm leading-5 text-left hover:bg-gray-500 hover:dark:bg-gray-700 hover:text-white" role="menuitem">Account settings</NavLink>
+                    <button type="button" onClick={() => dropdownItemClick(`/user/${userID}`)} className="flex justify-between w-full px-4 py-2 text-sm leading-5 text-left hover:bg-gray-500 hover:dark:bg-gray-700 hover:text-white" role="menuitem">View profile</button>
+                    <button type="button" onClick={() => dropdownItemClick('userSettings')} className="flex justify-between w-full px-4 py-2 text-sm leading-5 text-left hover:bg-gray-500 hover:dark:bg-gray-700 hover:text-white" role="menuitem">Account settings</button>
                   </div>
                   <div className="py-1">
                     <a href="/" className="flex justify-between w-full px-4 py-2 text-sm leading-5 text-left hover:bg-gray-500 hover:dark:bg-gray-700 hover:text-white" onClick={signOut} role="menuitem">Sign out</a>
